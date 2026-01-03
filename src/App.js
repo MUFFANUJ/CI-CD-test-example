d42ff15c460c54963aee812908f40633f06241a2
ANUJ KUMAR SINGH <workwithanuj26@gmail.com>
2026-01-03 17:34:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div>
-        <h1>Welcome to My App</h1>
+        <h1>Welcome </h1>
         <p>This is a simple React component with buttons.</p>
         <p>Click the buttons below to interact:</p>
 
